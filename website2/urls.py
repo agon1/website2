c1b403f9a5d9a0a232a8db80abf7581f7c141c56
agon <masterthe844@gmail.com>
2018-04-30 10:07:51 +0000

    Testum Github

--- website2/urls.py
+++ website2/urls.py
@@ -1,8 +1,8 @@
-
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
-    path(r'^music/', admin.site.urls),
-    path(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^music/', include('music.urls')),
+
 ]
