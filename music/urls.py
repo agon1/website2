682951ba3ab7e2770640e674a792bea15b342ab7
agon <masterthe844@gmail.com>
2018-05-14 01:41:23 +0000

    Testum Github

--- music/urls.py
+++ music/urls.py
@@ -5,11 +5,21 @@ app_name = 'music'
 
 urlpatterns = [
     # /music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
-    # /music/712/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
 
-    # /music/<album_id>/favorite/
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    # /music/712/<album_id>/
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailsView.as_view(), name='detail'),
+
+
+    # /music/album/add/
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    # /music/album/2/
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    # /music/album/2/
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
+
 ]
